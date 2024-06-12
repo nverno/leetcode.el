17312dc2e6e31c6ce6ed8583d75fc48050ae99f5
Noah Peart <noah.v.peart@gmail.com>
2024-06-12 09:13:40 -0700

    New customization options and refactoring
    New user customization options:
    * leetcode-before-submit-hook: run before submissions
    * leetcode-after-set-language-hook: run after leetcode--lang is set when
      starting to code
    * leetcode-buffer-code-function: to allow customizing code to submit
    * leetcode-filename-function: to allow customizing title-slugs to
      filename conversion
    * leetcode-display-configuration: customize layout of problem buffers
    
    API:
    * converted most api calls to sync - still async for leetcode-try and
      leetcode-submit which poll the check endpoint. Added
      leetcode-refresh-fetch-async for async problem list refresh.
    * removed aio dependency
    * all testcases inserted when test buffers are created
    * interactive functions that use the current problem-id can be called in
      any of the problem buffers (code, test, detail, result)
    
    Refactoring:
    * Keep title-slug in problem struct so none of the slugifying stuff is
      necessary anymore
    * leetcode-buffers: new struct to manage problem buffers (code, detail,
      test, result)
    * simplified window display stuff: windows kept in single alist
    
    Other:
    * leetcode-result-mode: major mode in results buffer. Adds compilation
      error support and some font-locking
    * completing read for problems: most interactive functions use this
    * removed no-byte-compile so leetcode.el is compiled

--- test/leetcode-tests.el
+++ test/leetcode-tests.el
@@ -24,34 +24,27 @@
 
 (require 'leetcode)
 
-(ert-deftest leetcode-test-slugify-title ()
-  (should (equal (leetcode--slugify-title "Two Sum") "two-sum"))
-  (should (equal (leetcode--slugify-title "Reverse Nodes in k-Group") "reverse-nodes-in-k-group"))
-  (should (equal (leetcode--slugify-title "Pow(x, n)") "powx-n"))
-  (should (equal (leetcode--slugify-title "Implement strStr()") "implement-strstr"))
-  (should (equal (leetcode--slugify-title "String to Integer (atoi)") "string-to-integer-atoi")))
-
 (ert-deftest leetcode-test-fetch-csrftoken ()
   (should (equal (leetcode--csrf-token) (leetcode--csrf-token))))
 
-(ert-deftest leetcode-test-login ()
-  (let ((leetcode-account
-         (with-temp-buffer
-           (insert-file-contents "test-account")
-           (car (split-string (string-trim (buffer-string)) "\n"))))
-        (leetcode-password
-         (with-temp-buffer
-           (insert-file-contents "test-account")
-           (cadr (split-string (string-trim (buffer-string)) "\n")))))
-    (setq leetcode--user nil)
-    (setq leetcode--problems nil)
-    (should (equal (leetcode--login-p) nil))
-    (deferred:sync! (leetcode--login leetcode-account leetcode-password))
-    (deferred:sync! (leetcode-problems-refresh))
-    (should (equal (not (leetcode--login-p)) nil))
-    (setq leetcode--user nil)
-    (setq leetcode--problems nil)
-    (kill-buffer (get-buffer leetcode--buffer-name))))
+;; (ert-deftest leetcode-test-login ()
+;;   (let ((leetcode-account
+;;          (with-temp-buffer
+;;            (insert-file-contents "test-account")
+;;            (car (split-string (string-trim (buffer-string)) "\n"))))
+;;         (leetcode-password
+;;          (with-temp-buffer
+;;            (insert-file-contents "test-account")
+;;            (cadr (split-string (string-trim (buffer-string)) "\n")))))
+;;     (setq leetcode--user nil)
+;;     (setq leetcode--problems nil)
+;;     (should (equal (leetcode--login-p) nil))
+;;     (deferred:sync! (leetcode--login leetcode-account leetcode-password))
+;;     (deferred:sync! (leetcode-problems-refresh))
+;;     (should (equal (not (leetcode--login-p)) nil))
+;;     (setq leetcode--user nil)
+;;     (setq leetcode--problems nil)
+;;     (kill-buffer (get-buffer leetcode--buffer-name))))
 
 (ert-deftest leetcode-test-make-tabulated-headers ()
   (should (equal (leetcode--make-tabulated-headers
@@ -63,17 +56,6 @@
                     ["✓" "5"    "Longest Palindromic Substring" "27.0%" "medium"]
                     [" " "6"    "ZigZag Conversion" "31.3%" "medium"]))
                  [(" " 1 nil) ("#" 1 nil) ("Problem" 46 nil) ("Acceptance" 10 nil) ("Difficulty" 10 nil)])))
-
-(ert-deftest leetcode-test-get-code-buffer-name ()
-  (let ((leetcode-prefer-language "c"))
-    (should (equal (leetcode--get-code-buffer-name "String to Integer (atoi)")
-                   "string-to-integer-atoi.c")))
-  (let ((leetcode-prefer-language "java"))
-    (should (equal (leetcode--get-code-buffer-name "Pow(x, n)")
-                   "powx-n.java")))
-  (let ((leetcode-prefer-language "python3"))
-    (should (equal (leetcode--get-code-buffer-name "Reverse Nodes in k-Group")
-                   "reverse-nodes-in-k-group.py"))))
 
 (provide 'leetcode-tests)
 ;;; leetcode-tests.el ends here
